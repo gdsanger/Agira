b29d9d342a58ba302e2d5f2c7f4460e0d2310250
Christian Angermeier <ca@perculasoft.de>
2026-01-27 16:50:01 +0000

    dsa

--- agira/settings.py
+++ agira/settings.py
@@ -30,10 +30,12 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-zd-q-2@-+#(8wu#t=hsy2q236!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,agira.angermeier.net').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1,agira.angermeier.net,agira.isarlabs.de' ).split(',')
 CSRF_TRUSTED_ORIGINS = [
     "https://agira.angermeier.net:8443",
+    "https://agira.isarlabs.de",
     "http://localhost:8005",
+    "http://localhost:8006",
     "http://127.0.0.1"
 ]
 
@@ -51,7 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',           q                                                                                               
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
